0a1644ca008f5053aee3ceebf258f855e25e8f60
Jessica Stapp <stapp9@gmail.com>
2019-10-15 13:18:59 -0700

    redefine values and names of each question's answer on html page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,32 @@
-var clockRunning = false;
-var timesUp;
-//timesUp function
+// window.onload = function() {
+//   $("#start").on("click", start);
+// };
 
-//timeRemaining function to countdown the timer
+var intervalId;
+
+var clockRunning = false;
+var timeLeft;
+
+function start() {
+  if (!clockRunning) {
+    intervalId = setInterval(count, -1000);
+    clockRunning = true;
+    $(".time-remaining").text(timeLeft);
+  }
+}
+//timesUp function
+// var timesUp = function() {
+//     if ()
+// }
+// //timeRemaining function to countdown the timer
 
 //function to begin timer, start counting down the time and display the remaining time (and make questions visible!)
-$("#start").on("click", function() {
+
+if (timeLeft === 0) {
   timesUp = setTimeout(function() {
     alert("You don't know Cher and you have lost the game!");
-  }, 5000);
-  //$(".time-remaining").text(timeRemaining);
-});
+  }, 1000 * 60);
+}
 
 //function to turn off timer and alert = "you won!"
 //if all answers match the correct answers array
