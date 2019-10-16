11bd9644237071ced94b4b09e8c4a1fdb8ff2ceb
Jessica Stapp <stapp9@gmail.com>
2019-10-16 13:51:42 -0700

    edit countdown function to change text when time is up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,6 +22,7 @@ function countdown() {
     stop();
     clockRunning = false;
     $("#finish").hide();
+    $("#time").text("You don't know Cher; you lose!");
   }
 }
 function reset() {
@@ -42,22 +43,10 @@ function finish() {
   }
 }
 
-//timesUp function
-// var timesUp = function() {
-//     if ()
-// }
-// //timeRemaining function to countdown the timer
-
-//function to begin timer, start counting down the time and display the remaining time (and make questions visible!)
-
 if (timeLeft === 0) {
   timesUp = setTimeout(function() {
     alert("You don't know Cher and you have lost the game!");
   }, 1000 * 60);
 }
-
-//function to turn off timer and alert = "you won!"
-//if all answers match the correct answers array
-//else (if timer runs out) "you lose!"
 
 //quiz resource code from stack overflow: https://stackoverflow.com/questions/28403558/multiple-choice-quiz-getting-score
